e6b1e2882ace5236cc114134d6b4cb092e2e5673
timkoers <timkoers@users.noreply.github.com>
2018-08-02 14:14:13 +0200

    Update Prepare_toolchain.sh
    Removed the conflicting package and updated to apt instread of apt-get

--- Prepare_toolchain.sh
+++ Prepare_toolchain.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-if ! hash apt-get 2>/dev/null; then
+if ! hash apt 2>/dev/null; then
 	whiptail --title "Orangepi Build System" --msgbox "This scripts requires a Debian based distrbution. If you not use Debian/Ubunut, pls install:[ bsdtar mtools u-boot-tools pv bc sunxi-tools gcc automake make qemu dosfstools ]"
 	exit 1
 fi
 
-apt-get -y --no-install-recommends --fix-missing install \
+apt -y --no-install-recommends --fix-missing install \
 	bsdtar mtools u-boot-tools pv bc \
 	gcc automake make \
 	lib32z1 lib32z1-dev qemu-user-static \
-	dosfstools libncurses5-dev lib32stdc++ debootstrap
+	dosfstools libncurses5-dev debootstrap
 
 # Prepare toolchains
 ROOT=`cd .. && pwd`
